e6a5662dc3b88feeefab4a7bfe540fecfa9dabd9
Federico Builes <federico.builes@gmail.com>
2009-01-29 12:00:24 +0100

    More specs for EventParser.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,27 +1,31 @@
+# -*- coding: utf-8 -*-
 require File.dirname(__FILE__) + "/spec_helper"
 
 describe "Parser" do
+  it 'should recognize the correct language' do
+    EventParser.new('this is an english text...').language.should == :english
+    EventParser.new('esto es español...').language.should == :spanish
+    EventParser.new('Dhr. Nepgast heeft maandag een...').language.should == :dutch
+  end
+
   it 'should downcase the text' do
-    EventParser.new('Some Interesting Text').downcase.should == 'some interesting text'
+    EventParser.new('Some Interesting Text').text == 'some interesting text'
+    EventParser.new('Un texto Interesante').text == 'un texto interesante'
   end
 
   it 'should replace number words with numbers' do
-    EventParser.new('one two three').replace_numbers.text.should == '1 2 3'
-    EventParser.new('thirty one').replace_numbers.text.should == '31'
-    EventParser.new('not a number').replace_numbers.text.should == ''
-  end
-
-  it 'should replace ordinal words with ordinal numbers' do
-    EventParser.new('third fourth').numericize.text.should == '3rd 4th'
-    EventParser.new('first second').numericize.text.should == '1st 2nd'
+    EventParser.new('one two three').text.should == '1 2 3'
+    EventParser.new('thirty one').text.should == '31'
+    EventParser.new('not a number').text.should == 'not a number'
   end
 
   it 'should remove unused words' do
-    text = 'Someone needs something on mondays at 4PM foo bar'
-    EventParser.new(text).strip_extra_words.text.should == 'Someone needs something on mondays at 4PM'
+    text = 'Some guy needs something on mondays at 4PM foo bar'
+    EventParser.new(text).text.should == 'Some guy needs something on mondays at 4PM'
   end
-  it 'should replace common words with hours' do
-    EventParser.new("morning noon").normalize_hours.text.should == '7:00 12:00'
-    EventParser.new("afternoon night").normalize_hours.text.should == '14:00 8:00'
+
+  it 'should replace common day times words with hours' do
+    EventParser.new("morning noon").text.should == '7:00 12:00'
+    EventParser.new("afternoon night").text.should == '14:00 20:00'
   end
 end
