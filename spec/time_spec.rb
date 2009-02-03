00103ab390b63d91cf9d6fccb8b16acc733620bd
Federico Builes <federico.builes@gmail.com>
2009-02-03 16:04:04 +0100

    Adding some specs for the Time patches.

---
+++ spec/time_spec.rb
@@ -0,0 +1,38 @@
+require File.dirname(__FILE__) + "/spec_helper"
+
+describe 'Time patches' do
+  before :each do
+    @date = DateTime.new(2009, 2, 1, 15, 49, 23)
+    @another_date = DateTime.new(2009, 2, 1, 16, 0, 23)
+  end
+
+  it 'should allow substraction of DateTime with DateTime' do
+    ret = @another_date - @date
+    ret.should == 11          # minutes
+  end
+
+  it 'should allow substraction of DateTime with Hours' do
+    ret = @date - 1.hour
+    ret.should == DateTime.strptime("2009-02-01T14:49:23+00:00")
+
+    # TODO failing, fix.
+    ret = @date - 24.hours
+    ret.should == DateTime.strptime("2009-01-31T15:49:23+00:00").to_s
+  end
+
+  it 'should allow conversion from DateTime to Time' do
+    expected = Time.local( 2009, 2, 1, 15, 49, 23)
+    @date.to_time.should == expected
+  end
+
+  it 'should allow sum of DateTime with Hours' do
+    ret = @date + 1.hour
+    ret.should == DateTime.new(2009, 2, 1, 16, 49, 23)
+  end
+
+  it 'should not allow sum of DateTime with DateTime' do
+    lambda { 
+      @date + @another_date
+    }.should raise_error
+  end
+end
