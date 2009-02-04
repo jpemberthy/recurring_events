6594b0b029d91a9fd0f9d81a9308e9e5c90f533f
Federico Builes <federico.builes@gmail.com>
2009-02-04 11:04:12 +0100

    Cleaning up parser_english_spec.rb

--- spec/parsers/parser_english_spec.rb
+++ spec/parsers/parser_english_spec.rb
@@ -1,19 +1,15 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe 'English Parser' do
-  before :all do
-    @parser = EventParser
-  end
-
   it "should recognize simple sentences with hour" do
     EventParser.parse('mr test needs a visit at 3:00pm').should_not be_nil
   end
 
   it 'should recognize sentences with week days and a time' do
-    @parser.parse("Mr. Fakedude needs a visit on monday morning.").should_not be_nil
+    EventParser.parse("Mr. Fakedude needs a visit on monday morning.").should_not be_nil
   end
 
   it 'should recognize sentences with custom events' do
-    @parser.parse("Mr. Fakedude needs a chicken soup every day at lunch.").should_not be_nil
+    EventParser.parse("Mr. Fakedude needs a chicken soup every day at lunch.").should_not be_nil
   end
 end
