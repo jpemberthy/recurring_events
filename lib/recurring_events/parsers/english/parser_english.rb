a3a7de011d06b7884ca76a74ae2df15cef25d5bb
Federico Builes <federico.builes@gmail.com>
2009-02-09 09:50:12 +0100

    Work around Treetop's annotations limitations by creating a generic module that can be extended by all modules.

--- lib/recurring_events/parsers/english/parser_english.rb
+++ lib/recurring_events/parsers/english/parser_english.rb
@@ -1,39 +1,80 @@
+# Treetop support for annotations (modules/classes below) is still
+# sub-optimal, requiring classes for certain cases and module extension for
+# others. Instead of worrying about what has to be a class and what has to be
+# a module (found mostly by trial/error), we'll create a generic module
+# called ClassParserNode that includes the #new method. Then we can replace
+# our classes with modules and we just have to extend the ClassParserNode
+# that will provide the #new method when necessary.
+#
+# Example:
+#
+#     module MyParserNode
+#       extend ClassParserNode
+#       def value
+#         ""
+#       end
+#     end
+# 
+# With this workaround we can safely skip the class/module distinction in
+# Treetop until that's fixed. For more information read:
+# http://groups.google.com/group/treetop-dev/browse_thread/thread/51d18f3a19daf7d
+
+# Generic Node for Treetop's parsing weirdness. Extend this module in your
+# own module instead of creating classes/modules at random.
+module ClassParserNode
+  def new(*args)
+    Treetop::Runtime::SyntaxNode.new(*args).extend(self)
+  end
+end
+
 # Full phrase.
-class PhraseNode < Treetop::Runtime::SyntaxNode # :nodoc:
+module PhraseNode
+  extend ClassParserNode
+
   def value
     subject.value.merge(predicate.value)
   end
 end
 
 # Phrase after stripping the subject.
-class PredicateNode < Treetop::Runtime::SyntaxNode # :nodoc:
+module PredicateNode
+  extend ClassParserNode
+
   def value
     event.value.merge(time_phrase.value)
   end
 end
 
 # Subject of the phrase.
-class SubjectNode < Treetop::Runtime::SyntaxNode # :nodoc:
+module SubjectNode
+  extend ClassParserNode
+
   def value
     { :subject => text_value }
   end
 end
 
-class TimePhraseNode < Treetop::Runtime::SyntaxNode # :nodoc:
+module TimePhraseNode
+  extend ClassParserNode
+
   def value
     time.value
   end
 end
 
 # Event time including date and hour.
-module TimeNode# < Treetop::Runtime::SyntaxNode # :nodoc:
+module TimeNode
+  extend ClassParserNode
+
   def value
     { :time => text_value }
   end
 end
 
 # Type of event.
-class EventNode < Treetop::Runtime::SyntaxNode # :nodoc:
+module EventNode
+  extend ClassParserNode
+
   def value
     { :event => text_value }
   end
