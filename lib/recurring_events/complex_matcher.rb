306d285b64e53c135d4ba5cb73dfec39e5a96af3
Federico Builes <federico.builes@gmail.com>
2009-02-17 16:36:21 +0100

    Adding docs for the matchers.

--- lib/recurring_events/complex_matcher.rb
+++ lib/recurring_events/complex_matcher.rb
@@ -1,6 +1,12 @@
 class ComplexMatcher
   attr_reader :name, :regexp, :proc
 
+  # Creates a new ComplexMatcher with a name, a regexp and a Proc. All the
+  # fields are mandatory. If you want to skip the Proc then you can use a
+  # SimpleMatcher.
+  # 
+  # proc will be saved and applied to the text if the text matches the regexp
+  # in a match call.
   def initialize(name, regexp, proc)
     @name = name
     @regexp = regexp
@@ -9,6 +15,10 @@ class ComplexMatcher
     validate_input
   end
 
+  # Matches the text with the matcher's regular expression. If it matches
+  # then it will apply the Proc to the text and return an array containing
+  # [new_text, category] where category is the name of this matcher. Returns
+  # nil if the text does not match.
   def match(text)
     if @regexp.match(text)
       [@proc.call(text), @name]
@@ -19,6 +29,7 @@ class ComplexMatcher
 
   private
   
+  # make sure we don't get empty data.
   def validate_input
     raise ComplexMatcherError.new("name can't be nil") if @name.nil?
     raise ComplexMatcherError.new("regexp can't be nil") if @regexp.nil?
--- lib/recurring_events/simple_matcher.rb
+++ lib/recurring_events/simple_matcher.rb
@@ -1,12 +1,25 @@
 class SimpleMatcher
   attr_reader :name, :regexp
 
+  # Creates a new SimpleMatcher with name and a regexp. Both fields are
+  # mandatory. If you want to apply a Proc to the resulting expression (to
+  # transform it) then take a look at ComplexMatcher.
+  # 
+  # The matcher's name represents the text category it matches. An example of
+  # this:
+  # 
+  #     SimpleMatcher.new(:time, /\d\d:\d\d/)
+  # A call to match is usually what follows the creation of the matcher.
   def initialize(name, regexp)
     @name = name
     @regexp = regexp
     validate_input
   end
 
+  # Matches the text against the regular expression provided in the matcher's
+  # creation. If it matches then it returns an array with [text, category] where
+  # category is the name of the matcher. Returns nil if the text does not
+  # match.
   def match(text)
     if @regexp.match(text)
       [text, @name]
@@ -16,7 +29,8 @@ class SimpleMatcher
   end
 
   private
-  
+
+  # make sure we don't get empty data
   def validate_input
     raise ComplexMatcherError.new("name can't be nil") if @name.nil?
     raise ComplexMatcherError.new("regexp can't be nil") if @regexp.nil?
