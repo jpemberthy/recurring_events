bb388e9ccacdd39aae38aeff9caba2060244a457
Federico Builes <federico.builes@gmail.com>
2009-02-17 14:28:38 +0100

    Docs.

--- lib/recurring_events/recoup.rb
+++ lib/recurring_events/recoup.rb
@@ -6,11 +6,22 @@ class Recoup
     setup_categories
   end
 
+  # Starts the parsing process for text. Returns an Event or nil if the text
+  # could not be parsed.
   def self.start(text)
     self.new(text).start
   end
 
-  def start
+  # Starts the parsing process. The steps are:
+  # 1. Run the text through the preprocessor which will return a clean
+  # tokenized output.
+  # 2. Match the tokens against the database records or against the custom
+  # filters.
+  # 3. ???
+  # 4. Profit!
+  #
+  # 
+  def start                     # :nodoc:
     to_match = []
 
     @processor.process.each do |token|
@@ -33,5 +44,4 @@ class Recoup
       @matches[category] = []
     end
   end
-
 end
