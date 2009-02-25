ecd57fc6e27eef7028b96fc125837e3f0b0efe3f
Federico Builes <federico.builes@gmail.com>
2009-02-25 16:38:50 +0100

    Refactoring recoup.rb

--- lib/recurring_events/recoup.rb
+++ lib/recurring_events/recoup.rb
@@ -6,10 +6,8 @@ class Recoup
     @processor = Preprocessor.new(text)
     @matches = { }
     @to_match = []
-    setup_categories
 
-    corpus = File.join(File.dirname(__FILE__), "corpus.yml")
-    db.load_yaml(corpus)
+    init_recoup
   end
 
   # Starts the parsing process for text. Returns an Event or nil if the text
@@ -24,75 +22,70 @@ class Recoup
   # 2. Match the tokens against the database records or against the custom
   # filters.
   # 3. Save the non-matched words in a database (with the full phrase).
-  # 4. ??
-  # 5. Returns a hash containing the extracted properties.
+  # 4. Returns a hash containing the extracted properties.
   def start                     # :nodoc:
     run_corpus
-    run_matchers if !@to_match.empty?
+    run_matchers
     save_unmatched_words if !@to_match.empty?
     @matches
   end
   
   protected
   
-  # creates the initial keys for every category in @matches
+  # Runs the list of tokens through the corpus and saves away the ones it
+  # recognizes. Non-matched tokens will be processed by the matchers.
+  def run_corpus                # :nodoc:
+    tokens = @processor.process
+    tokens.each do |token|
+      category = @db[token]
+      @matches[category] << token if category # word's in corpus.
+    end
+  end
+
+  # Runs the unmatched tokens through the custom matchers and
+  # saves the ones it recognizes.
+  def run_matchers              # :nodoc:
+    phrase = remove_matched(@processor.original_text.clone.downcase)
+    matches = Matchers.run(phrase)
+
+    matches.each do |match|
+      text, category = match
+      if text
+        @matches[category] << text
+      else
+        @to_match << text
+      end
+    end
+  end
+
+  # Save all the unmatched tokens to a database. Uses (token : original
+  # phrase) pairs. 
+  def save_unmatched_words      # :nodoc:
+    db = Corpus.new("unmatched.db")
+    text = @processor.original_text
+    @to_match.each { |token| db[token] = text }
+    db.close
+  end
+
+  # Receives a string and removes all the words in @matched from the it.
+  def remove_matched(string)
+    regexp = @matches.values.flatten.join("|")
+    string.gsub(regexp, '')
+  end
+
+  # TODO: Need a practical way to set this up.
+  # Creates the initial keys for every category in @matches
   def setup_categories          # :nodoc:
-    [:event, :time, :subject, :salutation, :recurrency].each do |category|
+    [:event, :time, :salutation, :recurrency, :preposition,
+     :article, :day, :verb, :name, :number].each do |category|
       @matches[category] = []
     end
   end
 
-  # Runs the list of tokens through the corpus and saves away the ones it
-  # recognizes. Non-matched tokens go into @to_match to pass them through the
-  # matchers.
-  def run_corpus                # :nodoc:
-    @processor.process.each do |token|
-      category = @db[token]
-      if category               # the word is in the corpus
-        @matches[category] ||= []
-        @matches[category] << token
-      else                      # we'll need to run it through the matchers
-        @to_match << token
-      end
-    end
-  end
-
-  # Runs the @to_match list of tokens through the different matchers and
-  # saves the ones it recognizes in @matches.
-  def run_matchers              # :nodoc:
-    phrase = remove_matched(@processor.original_text.clone)
-    Matchers.load_default_matchers
-    matches = Matchers.run(phrase)
-    matches.each do |match|
-      text,category = match
-      if !text.nil?
-        @matches[category] ||= []
-        @matches[category] << text
-        phrase.sub(text, '')
-      else
-        @to_match << text
-
-    end
-    end
-  end
-
-  # Write all the words in @to_match to a database. Saves them as 
-  # (token : original phrase) pairs. Useful for future improvementes.
-  def save_unmatched_words      # :nodoc:
-    db = Corpus.new("unmatched.db")
-    text = @processor.original_text
-    @to_match.each do |token|
-      db[token] = text
-    end
-    db.close
-  end
-
-  def remove_matched(string)
-    @matches.each do |match_array|
-      match_array.last.each do |match|
-        string.gsub!(match, '')
-      end
-    end
-    string
+  # Initialization routine, sets up the corpus and the matchers.
+  def init_recoup
+    setup_categories
+    db.load_yaml(File.join(File.dirname(__FILE__), "corpus.yml"))
+    Matchers.load_default_matchers    
   end
 end
