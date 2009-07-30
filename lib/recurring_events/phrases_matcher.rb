a8e937bc49091a4994cee850fcb5953d0416229f
jpemberthy <jpemberthy@gmail.com>
2009-07-30 17:26:50 -0500

    phrases matcher, maybe I should change that name.

---
+++ lib/recurring_events/phrases_matcher.rb
@@ -0,0 +1,40 @@
+class PhrasesMatcher
+  attr_reader :result
+  
+  def initialize(result)
+    @result = result
+  end
+  
+  def answer
+    weights = phrases_weight
+    PhraseSet.message_for(heavier_weight(weights))
+  end
+  
+  #returns the id of the matched phrase
+  def heavier_weight(weights)
+    heavier_id = 0
+    heavier = 0
+    weights.each{|k,v| v > heavier ? (heavier_id = k; heavier = v) : continue }
+    heavier_id
+  end
+  
+  def phrases_weight
+    phrases = PhraseSet::PHRASES
+    matched_weights = Hash.new(0)
+    @result.each{|k,v| 
+      if v.class.name == "Array"
+        v.collect{|token| 
+          phrases.each{|phrase|
+            phrase.each{|pk, pv|
+              pv.collect{|keyword|
+                matched_weights[pk] += 1 if /keyword/ =~ token
+              }
+            }
+          }
+        }
+      end 
+    }
+    matched_weights
+  end
+  
+end
---
+++ spec/phrases_matcher_spec.rb
@@ -0,0 +1,12 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe PhrasesMatcher do
+  before(:each) do
+    @matcher = PhrasesMatcher.new
+  end
+   
+  #given a parsed text, it should find the phrase's answer that matches the text.
+  it "given a valid phrase it should find an answer" do
+  end
+  
+end
